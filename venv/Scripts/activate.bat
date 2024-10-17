292f39e6dd3438913b3653c220df97813fb2b7a8
whamoudDev <hamoudwalid57@gmail.com>
2024-10-17 12:01:06 +0200

    update environment

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Workspace\SynapsiaWorkspace\Spotijob\api_wha\venv
+set VIRTUAL_ENV=D:\Workspace\WalidWorkspace\FastApiTest\api_wha\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
